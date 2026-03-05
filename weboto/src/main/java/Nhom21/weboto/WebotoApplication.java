f7fc2d98dcaabce666b1c942957111e542acae5a
Ngô Đình Bảo <ngodinhbao04@gmail.com>
2026-03-05 14:35:20 +0700

    first commit

---
+++ weboto/src/main/java/Nhom21/weboto/WebotoApplication.java
@@ -0,0 +1,13 @@
+package Nhom21.weboto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebotoApplication.class, args);
+	}
+
+}
---
+++ weboto/src/test/java/Nhom21/weboto/WebotoApplicationTests.java
@@ -0,0 +1,13 @@
+package Nhom21.weboto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
